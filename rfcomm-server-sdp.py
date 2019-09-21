96a7fb0f0d42bb927aea7701e2431b427383e937
Zulfikar <zulfikar97@gmail.com>
2019-09-22 01:28:35 +0700

    Remove import from btcommon

--- rfcomm-server-sdp.py
+++ rfcomm-server-sdp.py
@@ -4,24 +4,23 @@
 #
 # $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
 
-from bluetooth import *
-from bluetooth.btcommon import SERIAL_PORT_CLASS, SERIAL_PORT_PROFILE
+import bluetooth as bt
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+server_sock = bt.BluetoothSocket(bt.RFCOMM)
+server_sock.bind(("", bt.PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
+bt.advertise_service(server_sock, "SampleServer",
+                     service_id=uuid,
+                     service_classes=[uuid, bt.SERIAL_PORT_CLASS],
+                     profiles=[bt.SERIAL_PORT_PROFILE],
+                     #   protocols = [ OBEX_UUID ]
+                     )
+
 print("Waiting for connection on RFCOMM channel %d" % port)
 
 client_sock, client_info = server_sock.accept()
@@ -30,7 +29,8 @@ print("Accepted connection from ", client_info)
 try:
     while True:
         data = client_sock.recv(1024)
-        if len(data) == 0: break
+        if len(data) == 0:
+            break
         print("received [%s]" % data)
 except IOError:
     pass
