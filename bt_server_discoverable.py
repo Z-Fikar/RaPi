0a7f5fcc102021891534f3c745644a9aa9cfdcfe
Zulfikar <zulfikar97@gmail.com>
2019-09-21 20:13:27 +0700

    Add log

--- bt_server_discoverable.py
+++ bt_server_discoverable.py
@@ -2,5 +2,7 @@ import subprocess as sp
 import time as t
 
 sp.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+print("BLUETOOTH: ON")
 t.sleep(10)
 sp.call(['sudo', 'hciconfig', 'hci0', 'noscan'])
+print("BLUETOOTH: OFF")
