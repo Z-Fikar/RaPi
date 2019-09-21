84904e168540a4fa7bc33cfbf63ae7b8de4540c7
Zulfikar <zulfikar97@gmail.com>
2019-09-22 03:16:14 +0700

    Lagi2 l2cap

--- l2capserver.py
+++ l2capserver.py
@@ -8,8 +8,8 @@ server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 
 port = 0x1001
 
-server_sock.bind(("", port))
-server_sock.listen(1)
+server_sock.l2cap_bind(("", port))
+server_sock.l2cap_listen()
 
 #uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ef"
 # bluetooth.advertise_service( server_sock, "SampleServerL2CAP",
@@ -17,16 +17,16 @@ server_sock.listen(1)
 #                   service_classes = [ uuid ]
 #                    )
 
-client_sock, address = server_sock.accept()
+client_sock, address = server_sock.l2cap_accept()
 print("Accepted connection from ", address)
 
-data = client_sock.recv(1024)
+data = client_sock.l2cap_recv(1024)
 print("Data received: ", str(data))
 
 while data:
-    client_sock.send('Echo => ' + str(data))
-    data = client_sock.recv(1024)
+    client_sock.l2cap_send('Echo => ' + str(data))
+    data = client_sock.l2cap_recv(1024)
     print("Data received:", str(data))
 
-client_sock.close()
-server_sock.close()
+client_sock.l2cap_close()
+server_sock.l2cap_close()
